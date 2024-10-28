8bfcd7073c6868abe1d8683715c03b617a6d1707
rileyvlmu <rvegting@lion.lmu.edu>
2024-10-28 12:59:59 -0700

    now hosted on firebase

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'blog-starter-app',
+  location: 'us-central1'
+};
+
