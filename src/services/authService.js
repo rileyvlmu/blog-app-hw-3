65564cec8603f7a74346f4c98e421c3d54e8b93e
rileyvlmu <rvegting@lion.lmu.edu>
2024-12-09 18:19:26 -0800

    final changes

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,26 +1,35 @@
-import { useState, useEffect } from "react"
-import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth"
-import { auth } from "../components/firebaseConfig"
+import { useState, useEffect } from "react";
+import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { auth } from "../components/firebaseConfig";
+import React from "react";
 
 export function SignIn() {
-    return <button onClick={() => signInWithPopup(auth, new GoogleAuthProvider())}>Sign In</button>
+    return React.createElement(
+        "button",
+        { onClick: () => signInWithPopup(auth, new GoogleAuthProvider()) },
+        "Sign In"
+    );
 }
 
 export function SignOut() {
-    return (
-        <div>
-            Hello, {auth.currentUser.displayName} &nbsp;
-            <button onClick={() => signOut(auth)}>Sign Out</button>
-        </div>
-    )
+    return React.createElement(
+        "div",
+        null,
+        `Hello, ${auth.currentUser.displayName} `,
+        React.createElement(
+            "button",
+            { onClick: () => signOut(auth) },
+            "Sign Out"
+        )
+    );
 }
 
 export function useAuthentication() {
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null);
     useEffect(() => {
         return auth.onAuthStateChanged((user) => {
-            user ? setUser(user) : setUser(null)
-        })
-    }, [])
-    return user
+            user ? setUser(user) : setUser(null);
+        });
+    }, []);
+    return user;
 }
